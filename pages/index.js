bc9b71349068e68b38027ca30112e3d147859952
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-05 03:43:06 +0200

    Add Home page component

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Page } from '../components/layout'
+
+const Home = () => {
+  return (
+    <Page title='Home Page'>
+      <h1>Dev Connector Next</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa debitis, molestiae quos,
+        natus quaerat tempora nostrum excepturi voluptas laudantium, placeat neque unde voluptates.
+        Eveniet, illum! Ipsum dignissimos praesentium in suscipit.
+      </p>
+    </Page>
+  )
+}
+
+export default Home
