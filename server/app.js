be18acf736df9e71c0dedac7bbcfd773056430c5
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-10 13:03:24 +0200

    Update app.js file; add routes for users endpoints

--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ server.set('trust proxy', true)
 
 server.use(session)
 
+server.use('/api/users', require('./routes/user'))
+
 server.use((err, _req, res, _next) => {
   const { status = 500, message } = err
   res.status(status).json(message)
