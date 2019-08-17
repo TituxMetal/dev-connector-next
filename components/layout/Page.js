588a7e46efc7c714f981f04bf696b8b05fc27347
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-17 09:26:56 +0200

    Update the Page component; integrate the navigation Menu and Footer components

--- components/layout/Page.js
+++ components/layout/Page.js
@@ -1,15 +1,37 @@
-import { HtmlHead } from '../layout'
-import { GlobalStyle } from '../../styled'
+import styled from 'styled-components'
 
-const Page = ({ children, title }) => (
-  <>
+import { HtmlHead } from '../layout'
+import { Menu } from '../navigation'
+import { GlobalStyle, Footer } from '../../styled'
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Container = styled.section`
+  flex-grow: 1;
+  margin: auto;
+  padding: ${props => !props.fullWidth && '1rem'};
+  width: 100%;
+
+  @media screen and (min-width: 600px) {
+    padding: ${props => !props.fullWidth && '2rem'};
+    width: ${props => !props.fullWidth && '75vw'};
+  }
+`
+
+const Page = ({ children, full, title }) => (
+  <Main>
     <GlobalStyle />
     <HtmlHead title={title} />
-    <>{children}</>
-    <>
+    <Menu />
+    <Container fullWidth={full}>{children}</Container>
+    <Footer>
       <p>Created with love by TuxiMetal</p>
-    </>
-  </>
+    </Footer>
+  </Main>
 )
 
 export default Page
