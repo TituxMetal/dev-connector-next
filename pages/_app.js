08173771c80d8c6a227d3cfa03aac85c36ef46f8
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-20 19:36:44 +0200

    Update _app.js file; add ContextProvider and check the user authStatus

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,38 @@
 import App, { Container } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
+import { ContextProvider } from '../context'
+import { getSessionFromClient, getSessionFromServer } from '../lib'
 import { theme } from '../styled'
 
 class MyApp extends App {
   static async getInitialProps ({ Component, router, ctx }) {
     let pageProps = {}
 
+    const authStatus = !ctx.req ? await getSessionFromClient() : await getSessionFromServer(ctx.req)
+
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    return { pageProps }
+    return { pageProps, authStatus }
   }
 
   render () {
-    const { Component, pageProps } = this.props
+    const {
+      Component,
+      authStatus,
+      pageProps,
+      router: { pathname }
+    } = this.props
 
     return (
       <Container>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <ContextProvider pathname={pathname} authStatus={authStatus}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ContextProvider>
       </Container>
     )
   }
