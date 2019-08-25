77cc3694ecc7628ea7ca70af8d4d3ecfddfb0b65
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-25 03:23:40 +0200

    Add profile route; POST /api/profiles

---
+++ server/routes/profile.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+const ProfileController = require('../controllers/profile')
+const { isAuthenticated, validateBody } = require('../middlewares')
+const { edit } = require('../validation/profiles')
+
+/*
+ @route     POST api/profiles
+ @desc      Add or edit the current logged in user profile
+ @params    company: string, website: string, location: string, status: string, skills: [string], bio: string, githubusername: string
+ @access    Private
+*/
+router.post('/', isAuthenticated, validateBody(edit), ProfileController.edit)
+
+module.exports = router
