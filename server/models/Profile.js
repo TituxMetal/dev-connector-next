b25c07ea4fcf280d70abb15794874381059e1602
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-08-28 10:47:04 +0200

    Update Profile model; add experience fields

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -16,7 +16,32 @@ const profileSchema = new mongoose.Schema(
       linkedin: { type: String },
       twitter: { type: String },
       facebook: { type: String }
-    }
+    },
+    experience: [
+      {
+        title: {
+          type: String,
+          required: true,
+          trim: true
+        },
+        company: {
+          type: String,
+          required: true,
+          trim: true
+        },
+        location: {
+          type: String,
+          trim: true
+        },
+        from: {
+          type: Date,
+          required: true
+        },
+        to: { type: Date },
+        current: { type: Boolean, default: false },
+        description: { type: String, trim: true }
+      }
+    ]
   },
   { timestamps: true }
 )
