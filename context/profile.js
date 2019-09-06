3df70145b86a0c7df119284b2d434fe7f101363d
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-09-06 09:58:53 +0200

    Update profile context; add profile and githubrepos states, to get a single profile and Github repositories

--- context/profile.js
+++ context/profile.js
@@ -1,19 +1,20 @@
 import { useState, createContext } from 'react'
 import Router from 'next/router'
 
-import { getAllProfiles } from '../lib'
+import { getAllProfiles, getProfileById, getGithubRepos } from '../lib'
 export const ProfileContext = createContext()
 
 export const ProfileProvider = ({ children }) => {
   const [profileList, setProfileList] = useState(null)
+  const [profile, setProfile] = useState(null)
+  const [githubrepos, setGithubrepos] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
 
-  const fetchProfiles = async () => {
+  const fetchProfiles = async (id = false) => {
     try {
-      const { data } = await getAllProfiles()
       setError(false)
-      setProfileList(data)
+      id ? await getProfile(id) : await getProfiles()
       setLoading(false)
     } catch (err) {
       const msg = (err.response.data && err.response.data.errors) || err.message
@@ -22,8 +23,36 @@ export const ProfileProvider = ({ children }) => {
     }
   }
 
+  const fetchGithubRepos = async githubusername => {
+    try {
+      const { data } = await getGithubRepos(githubusername)
+      setGithubrepos(data)
+    } catch (err) {
+      return (err.response && err.response.data) || err.message
+    }
+  }
+
+  const getProfiles = async () => {
+    const { data } = await getAllProfiles()
+    setProfileList(data)
+  }
+
+  const getProfile = async id => {
+    const { data } = await getProfileById(id)
+    setProfile(data)
+  }
+
   return (
-    <ProfileContext.Provider value={{ error, loading, profileList, fetchProfiles }}>
+    <ProfileContext.Provider
+      value={{
+        error,
+        githubrepos,
+        loading,
+        profile,
+        profileList,
+        fetchProfiles,
+        fetchGithubRepos
+      }}>
       {children}
     </ProfileContext.Provider>
   )
